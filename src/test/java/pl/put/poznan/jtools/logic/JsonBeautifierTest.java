c85937a4b1fc4d92ec535fd061eb545c5aebd55b
Maciej Wieczorek <you@example.com>
2022-12-17 16:39:16 +0100

    Usable testing code. First test partially implemented. Example json files added.

--- src/test/java/pl/put/poznan/jtools/logic/JsonBeautifierTest.java
+++ src/test/java/pl/put/poznan/jtools/logic/JsonBeautifierTest.java
@@ -4,18 +4,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JsonBeautifierTest {
+
+    static String projectRootDir = System.getProperty("user.dir");
+    static String resoursesDir = "src/test/resources";
+    static String examplesDir = "JsonExamples";
+    static String minifiedExamplesDir = "JsonMinifiedExamples";
+
     @Test
     public void testBeautify() {
-        Path exampleJson = Path.of("todo");
-        String stringJson = JsonFileReader.read(exampleJson);
-        String beautifiedJson = JsonBeautifier.beautify(stringJson);
+        Path minifiedJsonExample = Paths.get(projectRootDir, resoursesDir, minifiedExamplesDir, "minifiedexample1.json");
+        String minifiedJsonString = JsonFileReader.read(minifiedJsonExample);
+        String beautifiedJson = JsonBeautifier.beautify(minifiedJsonString);
 
-        Path exampleJsonBeautified = Path.of("todo");
-        JsonFileWriter.write(beautifiedJson, exampleJsonBeautified);
+        Path jsonExample = Paths.get(projectRootDir, resoursesDir, examplesDir, "example1.json");
+        String JsonString = JsonFileReader.read(jsonExample);
 
-        // TODO: use Junit
+        // System.out.println(beautifiedJson);
+
+        // TODO: use Junit and TextCompare
         assertTrue(true);
     }
 }
