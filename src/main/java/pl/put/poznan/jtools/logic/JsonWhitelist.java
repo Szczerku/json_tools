98693018f4c89c3a19372d5d5b6947750e43c130
Szczerku <93645221+Szczerku@users.noreply.github.com>
2022-12-18 19:05:02 +0100

    Whitelist fun

--- src/main/java/pl/put/poznan/jtools/logic/JsonWhitelist.java
+++ src/main/java/pl/put/poznan/jtools/logic/JsonWhitelist.java
@@ -3,20 +3,29 @@ package pl.put.poznan.jtools.logic;
 import java.util.List;
 
 public class JsonWhitelist extends JsonDecorator{
-    private final List<String> whitelist;
+    public final List<String> whitelist;
     public JsonWhitelist(JsonObjectInterface jsonObject, List<String> whitelist){
         super(jsonObject);
         this.whitelist = whitelist;
     }
-
     public String decorate() {
-        return decorateWhitelist();
+        return decorateWhitelist(super.decorate(), this.whitelist);
     }
-    public String decorateWhitelist(){
-        String jsonString = super.decorate();
+    public static String decorateWhitelist(String jsonString, List<String> white){
         // TODO: implement whitelisting
         // can access whitelist here, example: whitelist.get(0);
-
+        String[]arr = new String [white.size()];
+        //Converting List to Array
+        white.toArray(arr);
+        for (int i=0; i < arr.length; i++){
+            if(jsonString.contains(arr[i])) {
+                jsonString = jsonString.replaceAll(arr[i], Integer.toString(i));
+            }
+        }
+        jsonString = jsonString.replaceAll("[^0-9]","");
+        for (int j=0; j < arr.length; j++){
+            jsonString = jsonString.replaceAll(Integer.toString(j),arr[j]);
+        }
         // TODO: replace this with properly filtered string
         return jsonString;
     }
