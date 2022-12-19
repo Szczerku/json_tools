d970cfcb970ab1547273c20e46db98a9ab7ecdf7
Szczerku <93645221+Szczerku@users.noreply.github.com>
2022-12-19 12:25:15 +0100

    minify javadoc

--- src/main/java/pl/put/poznan/jtools/logic/JsonMinifier.java
+++ src/main/java/pl/put/poznan/jtools/logic/JsonMinifier.java
@@ -2,6 +2,9 @@ package pl.put.poznan.jtools.logic;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/***
+ * class designed for text processing with a focus on minify
+ */
 public class JsonMinifier extends JsonDecorator{
     public JsonMinifier(JsonObjectInterface jsonObject){
         super(jsonObject);
@@ -10,9 +13,16 @@ public class JsonMinifier extends JsonDecorator{
     public String decorate() {
         return decorateMinify();
     }
+
+    /***
+     * removes unnecessary characters from the code without changing the functionality
+     * parse method parses a JSON string, constructing the JavaScript value or object described by the string
+     * @return the parsed string
+     */
     public String decorateMinify() {
         String jsonString = super.decorate();
         JsonNode jsonNode = JsonParser.parse(jsonString);
         return jsonNode.toString();
     }
 }
+
