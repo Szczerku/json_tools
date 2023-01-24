9f73bb258e69bfbce183d30df719ffcdd956a0a2
Maciej Wieczorek <you@example.com>
2023-01-24 05:38:11 +0100

    Add GUI select, Add more tests for Blacklist

--- src/test/java/pl/put/poznan/jtools/logic/JsonBlacklistTest.java
+++ src/test/java/pl/put/poznan/jtools/logic/JsonBlacklistTest.java
@@ -1,4 +1,6 @@
 package pl.put.poznan.jtools.logic;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,15 +9,53 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class JsonBlacklistTest {
+
+    private ArrayList<String> blacklist;
+
+    @BeforeEach
+    public void setUp() {
+        blacklist = new ArrayList<String>();
+    }
+
     @Test
-    public void testDecorate(){
+    public void testDecorate1(){
         JsonObject jsonObject = mock(JsonObject.class);
         String jsonInString = "{\"widget\": {\"debug\": \"on\", \"window\": { \"title\": \"Sample Konfabulator Widget\" }}}";
         when(jsonObject.decorate()).thenReturn(jsonInString);
-        List<String> blacklist = new ArrayList<String>();
+
         blacklist.add("window");
+
         JsonBlacklist jsonBlacklist = new JsonBlacklist(jsonObject, blacklist);
         String expectedString = "{\"widget\":{\"debug\":\"on\"}}";
+
+        assertEquals(expectedString, jsonBlacklist.decorate());
+    }
+
+    @Test
+    public void testDecorate2(){
+        JsonObject jsonObject = mock(JsonObject.class);
+        String jsonInString = "{\"fruit\":\"Apple\",\"size\":\"Large\",\"color\":\"Red\"}";
+        when(jsonObject.decorate()).thenReturn(jsonInString);
+
+        blacklist.add("size");
+
+        JsonBlacklist jsonBlacklist = new JsonBlacklist(jsonObject, blacklist);
+        String expectedString = "{\"fruit\":\"Apple\",\"color\":\"Red\"}";
+
+        assertEquals(expectedString, jsonBlacklist.decorate());
+    }
+
+    @Test
+    public void testDecorate3(){
+        JsonObject jsonObject = mock(JsonObject.class);
+        String jsonInString = "{\"id\":\"0001\",\"type\":\"donut\",\"name\":\"Cake\",\"image\":{\"url\":\"images/0001.jpg\",\"width\":200,\"height\":200},\"thumbnail\":{\"url\":\"images/thumbnails/0001.jpg\",\"width\":32,\"height\":32}}";
+        when(jsonObject.decorate()).thenReturn(jsonInString);
+
+        blacklist.add("name");
+        blacklist.add("width");
+
+        JsonBlacklist jsonBlacklist = new JsonBlacklist(jsonObject, blacklist);
+        String expectedString = "{\"id\":\"0001\",\"type\":\"donut\",\"image\":{\"url\":\"images/0001.jpg\",\"height\":200},\"thumbnail\":{\"url\":\"images/thumbnails/0001.jpg\",\"height\":32}}";
 
         assertEquals(expectedString, jsonBlacklist.decorate());
     }
--- src/test/java/pl/put/poznan/jtools/logic/JsonFileReaderTest.java
+++
@@ -1,5 +0,0 @@
-package pl.put.poznan.jtools.logic;
-
-public class JsonFileReaderTest {
-
-}
